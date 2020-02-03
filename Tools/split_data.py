9772c50b2095f184fcdcc486f5d30e9217915d32
KennthShang <kenneth.shang@foxmail.com>
2020-02-03 17:17:12 +0800

    Update split_data.py

--- Tools/split_data.py
+++ Tools/split_data.py
@@ -1,17 +1,17 @@
 import numpy as np 
-
+import argparse
 
 parser = argparse.ArgumentParser(description='manual to this script')
-parser.add_argument('--input', type=str, default='validation/ebola.fasta')
+parser.add_argument('--input', type=str, default='validation/ebola.fa')
 args = parser.parse_args()
 
 raw_file = args.input
 
 
 data={}
-with open("result.txt") as file_in:
+with open("prediction/result.txt") as file_in:
     for line in file_in.readlines():
-        tmp = ilne.replace('\n', '').split("->")
+        tmp = line.replace('\n', '').split("->")
         id_ = int(tmp[0])
         class_ =int(tmp[1])
 
@@ -23,7 +23,15 @@ with open("result.txt") as file_in:
 with open(raw_file) as file_in:
     raw = file_in.readlines()
     for i in range(len(data.keys())):
-        with open("subset_"+str(i)+".fasta", 'w') as file_out:
+        with open("prediction/subset_"+str(i)+".fasta", 'w') as file_out:
             for item in data[i]:
-                file_out.write(raw[item])
+                file_out.write(raw[item-1])
 
+with open(raw_file) as file_in:
+    raw = file_in.readlines()
+    with open("prediction/early_stop.txt") as stop:
+        with open("prediction/early_stop.fasta", 'w') as file_out:
+            for line in stop.readlines():
+                id_ = int(line.replace('\n', ''))
+                file_out.write(raw[id_-1])
+            
