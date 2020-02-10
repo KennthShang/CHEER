9b5e821880e0694a1f243c2e5c50e55f8cc20251
Kenneth Shang <Kenneth.Shang@foxmail.com>
2020-02-10 14:55:24 +0800

    Update re_train_script.sh

--- Classifier/code/re_train_script.sh
+++ Classifier/code/re_train_script.sh
@@ -1,8 +1,8 @@
-python create_reads_train.py
-python create_reads_val.py
-python filter_reads_train.py
-python filter_reads_val.py
-python read2int_train.py
-python read2int_val.py
-python create_dataset_train.py
-python create_dataset_val.py
+python code/create_reads_train.py
+python code/create_reads_val.py
+python code/filter_reads_train.py
+python code/filter_reads_val.py
+python code/read2int_train.py
+python code/read2int_val.py
+python code/create_dataset_train.py
+python code/create_dataset_val.py
